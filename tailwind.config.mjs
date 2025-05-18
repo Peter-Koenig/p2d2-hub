70f86a697726db917e7b2ef25e35328baea7e15d
Peter König <web@dev.scanea.de>
2025-05-18 22:13:28 +0200

    Kacheln "Kategorien" angelegt. Kachel "Friedhöfe mit Interaktion versehen.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,20 @@ export default {
   content: ["./src/**/*.{astro,js,ts,jsx,tsx,md,mdx}"],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          50: "#eefff6",
+          100: "#d7ffea",
+          200: "#b2ffd6",
+          300: "#79ffb9",
+          400: "#3eff95",
+          500: "#16f079",
+          600: "#00cc5e",
+          700: "#00a04d",
+          800: "#007d40",
+          900: "#006737",
+        },
+      },
       fontFamily: {
         poppins: ["Poppins", ...fontFamily.sans],
         inter: ["Inter", ...fontFamily.sans],
